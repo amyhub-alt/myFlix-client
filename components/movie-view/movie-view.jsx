1655da9a45f3b150e15fe1339b5e9eedba458a88
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-10-14 13:08:43 -0400

    updated director name

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import "./movie-view.scss";
 
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -13,7 +14,7 @@ export const MovieView = ({ movie, onBackClick }) => {
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director.Name}</span>
+        <span>{movie.director}</span>
       </div>
       <div>
         <span>Genre: </span>
